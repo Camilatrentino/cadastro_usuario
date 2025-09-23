4dc2f27f983fed8e515aa02d78299d4247250ab0
camilatrentino <camila.f.trentino@gmail.com>
2025-09-23 12:33:53 -0300

    finalizado

--- cadastro_usuarios/src/pages/Home/index.jsx
+++ cadastro_usuarios/src/pages/Home/index.jsx
@@ -1,12 +1,13 @@
 
 import './style.css'
-import Lixeira from '../../assets/lixeira_peq.svg'
+import Lixeira from '../../assets/lixeira.png'
+import Lapis from '../../assets/fer.png'
 import { useEffect, useState, useRef } from 'react'
 import api from '../../services/api'
 
 function Home() {
    const [usuarios, setUsuarios] = useState([])
-
+   const [editandoId, setEditandoId] = useState(null)
    const inputNome = useRef()
    const inputEmail = useRef()
    const inputIdade = useRef()
@@ -15,23 +16,49 @@ function Home() {
     const usuariosDaApi = await api.get('/cadastro')
     setUsuarios(usuariosDaApi.data)
     console.log(usuarios)
-   }
-
-   async function createUsuarios(){
-    await api.post('/cadastro',{
-      email: inputEmail.current.value,
-      nome: inputNome.current.value,
-      idade: inputIdade.current.value
-    })
-    getUsuarios()
-   }
-
-   
+   }   
 
    useEffect(()=>{
     getUsuarios()
 
    },[])
+
+   function resetForm() {
+    inputNome.current.value = ''
+    inputEmail.current.value = ''
+    inputIdade.current.value = ''
+    setEditandoId(null)
+  }
+
+   async function createUsuarios(){
+    if (editandoId) {
+      await api.put(`/cadastro/${editandoId}`, {
+        email: inputEmail.current.value,
+        nome: inputNome.current.value,
+        idade: inputIdade.current.value
+      })
+    } else {
+      await api.post('/cadastro', {
+        email: inputEmail.current.value,
+        nome: inputNome.current.value,
+        idade: inputIdade.current.value
+      })
+    }
+    getUsuarios()
+    resetForm()
+  }
+
+   async function deleteUsuarios(id){
+    await api.delete(`/cadastro/${id}`)
+    getUsuarios()
+   }
+
+   function atualizaUsuarios(usuario) {
+    inputNome.current.value = usuario.nome;
+    inputEmail.current.value = usuario.email;
+    inputIdade.current.value = usuario.idade;
+    setEditandoId(usuario.id);
+  }
 
 
   return (
@@ -41,7 +68,7 @@ function Home() {
         <input placeholder='Digite seu nome' name='nome' type='text'  ref={inputNome}/>
         <input placeholder='Digite sua idade' name='idade' type='number' ref={inputIdade} />
         <input placeholder='Digite seu email' name='email' type='email' ref={inputEmail} />
-        <button type='button' onClick={createUsuarios}>Cadastrar</button>
+        <button type='button' onClick={createUsuarios}>{editandoId ? 'Atualizar' : 'Cadastrar'}</button>
       </form>
 
       {usuarios.map(usuario => (
@@ -51,8 +78,11 @@ function Home() {
             <p>Idade: <span>{usuario.idade}</span></p>
             <p>Email: <span>{usuario.email}</span></p>
           </div>
-          <button>
+          <button onClick={ ()=> deleteUsuarios(usuario.id)}>
             <img src={Lixeira} />
+          </button>
+          <button onClick={ ()=> atualizaUsuarios(usuario)}>
+            <img src={Lapis} />
           </button>
         </div>
       ))}
